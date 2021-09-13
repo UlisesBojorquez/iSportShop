c0f0ff75c7fb7ac8e4dd53a5dfb55e137285108f
UlisesBojorquez <ulibojorquez@gmail.com>
2021-09-13 17:22:04 -0600

    Commit first approach
    Modified activities:
    -Main
    -Register
    -Menu
    -Recover password
    
    Also, we add functionality about the register, log in and recover password.
    
    Communication with firebase, and manage of messages

--- app/src/main/java/com/example/isportshop/MenuActivity.kt
+++ app/src/main/java/com/example/isportshop/MenuActivity.kt
@@ -1,11 +1,27 @@
 package com.example.isportshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
+    public fun LogOut(v:View?){
+        auth.signOut()
+        val intent=Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
